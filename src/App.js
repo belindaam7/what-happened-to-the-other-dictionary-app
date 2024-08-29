019bfea7b59d5671cd408314db99fb4da375f58e
belindaam7 <bsamezquita@gmail.com>
2024-08-29 08:06:26 -0700

    Added API and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="container">
-      <div className="App">Hello from dictionary app</div>
+      <div className="App">
+        <Dictionary />
+      </div>
+      <footer>
+        This project was coded by Belinda Amezquita and it is open source 
+      </footer>
     </div>
   );
 }
